f701bf07b354e57de4e4279ea0b1546473ff936c
Öjvind Nilsson <ojvindnilsson@gmail.com>
2022-02-03 11:23:22 +0100

    Initial Setup
    Contains the J-- and Ant Source codes.
    
    Make sure to update PATH variables so that they work.

---
+++ j--/tests/fail/Division.java
@@ -0,0 +1,14 @@
+package fail;
+
+/**
+ * This class was made during: Lecture 1
+ * 
+ * @see https://learn.inside.dtu.dk/d2l/le/content/103833/viewContent/388889/View
+ * 
+ */
+public class Division {
+    
+    public static void main(String[] args) {
+        System.out.println('a' / 42);
+    }
+}
---
+++ j--/tests/junit/JMinusMinusTestRunner.java
@@ -0,0 +1,39 @@
+// Copyright 2013 Bill Campbell, Swami Iyer and Bahar Akbal-Delibas
+
+package junit;
+
+import java.io.File;
+import junit.framework.TestCase;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import pass.*;
+
+/**
+ * JUnit test suite for running the j-- programs in tests/pass.
+ */
+
+public class JMinusMinusTestRunner {
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite();
+        suite.addTestSuite(HelloWorldTest.class);
+        suite.addTestSuite(FactorialTest.class);
+        suite.addTestSuite(GCDTest.class);
+        suite.addTestSuite(SeriesTest.class);
+        suite.addTestSuite(ClassesTest.class);
+
+        // Start of the addition of new test cases
+        suite.addTestSuite(DivisionTest.class); // Lecture 1
+
+        return suite;
+    }
+
+    /**
+     * Runs the test suite using the textual runner.
+     */
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+}
