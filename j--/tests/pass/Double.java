f5b951c6bfa09d27bbb732da6658ac94ebe5bd54
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2022-03-17 11:36:06 +0100

    JavaCC: parse doubles

---
+++ j--/tests/pass/Double.java
@@ -0,0 +1,12 @@
+package pass;
+
+public class Double {
+    private double variable;
+
+    public double method(int argument) {
+        double a = 3.14;
+        double b = 1 + 2;
+        return a + b * (double)argument;
+    }
+
+}
