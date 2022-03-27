bd139f931c03c2c8da9060034ac071b0daf4b67f
Öjvind Nilsson <ojvindnilsson@gmail.com>
2022-03-27 16:03:22 +0200

    Merged Test From JakuJ
    double test file from

---
+++ j--/tests/pass/Double.java
@@ -0,0 +1,14 @@
+package pass;
+
+public class Double {
+
+    public double method(double argument) {
+        double a = 3.14;
+        double b = .14;
+        double c = 0.12;
+        double d = 1.;
+        double e = 1.000001;
+        return a + b - c / d * (double)2 + argument + e;
+    }
+
+}
